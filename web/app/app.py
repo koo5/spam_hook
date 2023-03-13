e42fa7384ea46b65e7e7612e47527146977dd14d
Your Name <you@example.com>
2023-03-13 15:03:47 +0100

    extracted all secrets to (/var/)secrets

--- web/app/app.py
+++ web/app/app.py
@@ -1,22 +1,20 @@
 """
 listen for github webhook and send commit body to discord webhook
+
+secrets/DISCORD_WEBHOOK_URL :
+server settings -> integrations -> webhooks -> new webhook -> webhook url
+
+secrets/GITHUB_SECRET :
+Webhooks -> add webhook -> 
+Payload URL: <your server>/discord_fulltext
+Content type: application/json
+Secret: GITHUB_SECRET
+
+
+
 """
 
 
-import os
-
-
-
-# settings
-
-# server settings -> integrations -> webhooks -> new webhook -> copy webhook url
-discord_webhook_url =  os.environ['DISCORD_WEBHOOK_URL']
-
-# Webhooks -> add webhook -> 
-#	Payload URL: <your server>/discord_fulltext
-#	Content type: application/json
-#	Secret: GITHUB_SECRET
-github_secret = os.environ['GITHUB_SECRET']
 
 
 from typing import Union
@@ -26,6 +24,13 @@ from hashlib import sha256
 import requests, json
 from enum import auto
 from fastapi_utils.enums import StrEnum
+import os
+
+
+def secret(name):
+	fn = os.environ.get('SECRETS_DIR','/run/secrets') + '/' + name
+	with open(fn, 'r') as x:
+		return x.read()
 
 
 class Mode(StrEnum):
@@ -33,8 +38,8 @@ class Mode(StrEnum):
 	fulltext = auto()
 
 
-
-
+# really curious if this will work
+os.environ['SENTRY_DSN'] = secret('SENTRY_DSN')
 import sentry_sdk
 
 
@@ -68,7 +73,7 @@ def verify_signature(x_hub_signature_256, body):
 	if x_hub_signature_256 is None: 
 		raise HTTPException(403, 'X-Hub-Signature-256 missing')
 	received_sign = x_hub_signature_256.split('sha256=')[-1].strip()
-	expected_sign = HMAC(key=github_secret.encode(), msg=body, digestmod=sha256).hexdigest()
+	expected_sign = HMAC(key=secret('GITHUB_SECRET').encode(), msg=body, digestmod=sha256).hexdigest()
 	if not compare_digest(received_sign, expected_sign):
 		raise HTTPException(403, 'X-Hub-Signature-256 invalid')
 
@@ -90,6 +95,14 @@ async def webhook(mode:Mode, request: Request, x_hub_signature_256: Union[str, N
 	return 'OK'
 
 
+@app.get('/spam/{secret_token}/{message}')
+async def webhook(secret_token: str, message: str):
+	if secret('SPAM_SECRET') == secret_token:
+		print(f'sending: {message}')
+		send_message(message)
+		return 'OK'
+
+
 @app.get("/sentry-debug")
 async def trigger_error():
     division_by_zero = 1 / 0
@@ -97,5 +110,5 @@ async def trigger_error():
 
 def send_message(message):
 	payload = {'content': message}
-	requests.post(discord_webhook_url, json=payload)
+	requests.post(secret('DISCORD_WEBHOOK_URL'), json=payload)
 
